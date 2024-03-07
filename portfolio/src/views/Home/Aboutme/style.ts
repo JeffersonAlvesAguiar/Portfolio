61c23c30642c1f8e60d7bcb321c33290be671282
Jefferon Alves de Aguiar <jeffoaguiar@gmail.com>
2024-03-06 21:12:05 -0300

    adicionando novo layout para "aboutme"

--- portfolio/src/views/Home/Aboutme/style.ts
+++ portfolio/src/views/Home/Aboutme/style.ts
@@ -5,11 +5,16 @@ width: 70%;
 height: 100vh;
 display: flex;
 align-items: center;
-
-/* background-color: aqua; */
 `;
 
 export const Span = styled.span`
 font-size: ${props => props.theme.font__size__100};
 line-height: 1.5;
+border-radius: 0.1rem;
+padding-top: 2rem;
+padding-right: 2rem;
+padding-bottom: 2rem;
+padding-left: 2rem;
+box-shadow: 0px 0px 5px 1px ${props => props.theme.blue__500};
+background-color: ${props => props.theme.white__100};
 `;
