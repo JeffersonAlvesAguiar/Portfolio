7abb029d7d80eca35fd84f62998dbdb2c7159455
Jefferon Alves de Aguiar <jeffoaguiar@gmail.com>
2024-03-11 18:52:02 -0300

    atualizando conteudo em "Aboutme"

--- Src/Views/Home/About/About.component.js
+++ Src/Views/Home/About/About.component.js
@@ -2,6 +2,6 @@ export const about = document.createElement('div');
 about.classList.add('container__about');
 
 const teste = document.createElement('span');
-teste.innerText = 'testando aboutme';
+teste.innerText = 'Olá! Seja bem-vindo! Estou na área de TI há dois anos. Ao longo dessa jornada, já trabalhei com JavaScript, TypeScript, React.js, Styled-components, Angular e SCSS. Tenho familiaridade com metodologias ágeis, como Scrum e Kanban. Tenho a intenção de me tornar um desenvolvedor Full Stack no futuro, e minha escolha para agregar no Back End é C# ASP.NET Core.';
 
 about.appendChild(teste);
