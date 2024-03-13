c6123870bc89aafe3812bf7b36f366a433b8bc0a
Jefferon Alves de Aguiar <jeffoaguiar@gmail.com>
2024-03-12 22:34:15 -0300

    implementando links em "Footer"

--- Src/Views/Home/Footer/Footer.component.js
+++ Src/Views/Home/Footer/Footer.component.js
@@ -7,7 +7,7 @@ box__author__footer.classList.add('box__author__footer');
 
 const author__footer = document.createElement('span');
 author__footer.classList.add('author__footer');
-author__footer.innerText = 'Jefferson Alves';
+author__footer.innerText = 'Desenvolvido por Jefferson Alves';
 
 box__author__footer.appendChild(author__footer);
 
@@ -15,11 +15,20 @@ box__author__footer.appendChild(author__footer);
 const box__contact__footer = document.createElement('div');
 box__contact__footer.classList.add('box__contact__footer');
 
-const contact__footer = document.createElement('span');
-contact__footer.classList.add('contact__footer');
-contact__footer.innerText = 'contato footer';
+const linkedin__footer = document.createElement('a');
+linkedin__footer.classList.add('contact__footer');
+linkedin__footer.href = 'https://www.linkedin.com/in/jefferson-alves-developer/';
+linkedin__footer.target = '_blank';
+linkedin__footer.innerText = 'Linkedin';
 
-box__contact__footer.appendChild(contact__footer);
+const gitHub__footer = document.createElement('a');
+gitHub__footer.classList.add('contact__footer');
+gitHub__footer.href = 'https://github.com/JeffersonAlvesAguiar';
+gitHub__footer.target = '_blank';
+gitHub__footer.innerText = 'Git Hub';
+
+box__contact__footer.appendChild(linkedin__footer);
+box__contact__footer.appendChild(gitHub__footer);
 
 // atribuindo alementos ao elemento pai
 footer.appendChild(box__author__footer);
